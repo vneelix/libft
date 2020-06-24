b8f02cda940dc0f15a3ce0a998a01db4a2955207
maxucheba@mail.ru <you@example.com>
2020-06-24 19:13:52 +0300

    direct

--- src/ft_directory.c
+++ src/ft_directory.c
@@ -6,72 +6,87 @@
 /*   By: vneelix <vneelix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 02:14:45 by vneelix           #+#    #+#             */
-/*   Updated: 2020/06/07 16:30:21 by vneelix          ###   ########.fr       */
+/*   Updated: 2020/06/24 19:12:01 by vneelix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	*ft_delete(char **nptr)
+void	*ft_delete(char **nptr)
 {
-	void *ptr;
+	char **temp;
 
-	ptr = nptr;
-	while (nptr != NULL && *nptr != NULL)
+	if (nptr == NULL)
+		return (NULL);
+	temp = nptr;
+	while (*nptr != NULL)
 	{
 		ft_memdel(*nptr);
 		nptr += 1;
 	}
-	ft_memdel(ptr);
+	ft_memdel(temp);
 	return (NULL);
 }
 
-static char	**add_file(char **file, char *n_file, size_t *num_files)
+size_t	ft_count(char *name)
 {
-	char			**ptr;
-	static size_t	count = 1;
-
-	if (file == NULL || n_file == NULL)
-	{
-		ft_delete(file);
-		ft_memdel(n_file);
-		return (NULL);
-	}
-	if ((ptr = ft_memalloc(sizeof(char*) * (count + 1))) == NULL)
-	{
-		ft_delete(file);
-		ft_memdel(n_file);
-		return (NULL);
-	}
-	ptr = ft_memcpy(ptr, file, sizeof(char*) * (count - 1));
-	ptr[count - 1] = n_file;
-	if (num_files != NULL)
-		*num_files = count;
-	count += 1;
-	ft_memdel(file);
-	return (ptr);
-}
-
-char		**ft_directory(char *name, size_t *num_files)
-{
+	size_t			i;
 	DIR				*dir;
-	char			**ret;
 	struct dirent	*entry;
 
-	if ((ret = (char**)ft_memalloc(sizeof(char*))) == NULL)
-		return (NULL);
 	if ((dir = opendir(name)) == NULL)
-		return (ft_delete(ret));
+		return (0);
+	i = 0;
+	while ((entry = readdir(dir)) != NULL)
+		if (entry->d_type == DT_REG)
+			i += 1;
+	closedir(dir);
+	return (i);
+}
+
+void	*ft_write(char **temp, DIR *dir, char *dir_name)
+{
+	size_t			i;
+	struct dirent	*entry;
+	char			*file_name;
+
+	i = 0;
 	while ((entry = readdir(dir)) != NULL)
 	{
-		if ((entry->d_name)[0] == '.')
+		if (entry->d_type != DT_REG)
 			continue ;
-		if ((ret = add_file(ret, ft_strdup(entry->d_name), num_files)) == NULL)
+		if ((file_name = ft_strjoin(dir_name, entry->d_name)) == NULL)
+			return (ft_delete(temp));
+		if ((temp[i] = realpath(file_name, NULL)) == NULL)
 		{
-			closedir(dir);
-			return (NULL);
+			ft_memdel(file_name);
+			return (ft_delete(temp));
 		}
+		ft_memdel(file_name);
+		i += 1;
 	}
+	return (temp);
+}
+
+char	**ft_directory(char *dir_name, size_t *num_files)
+{
+	DIR		*dir;
+	char	**ret;
+
+	if ((dir = opendir(dir_name)) == NULL)
+		return (NULL);
+	if ((*num_files = ft_count(dir_name)) == 0)
+	{
+		closedir(dir);
+		return (NULL);
+	}
+	if ((ret = (char**)ft_memalloc(sizeof(
+		void*) * (ft_count(dir_name) + 1))) == NULL)
+	{
+		closedir(dir);
+		return (NULL);
+	}
+	ret = ft_write(ret, dir, dir_name);
 	closedir(dir);
 	return (ret);
 }
