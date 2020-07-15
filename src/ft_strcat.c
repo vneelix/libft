16c8c6d8f075ea191e4410e33612845eac5ec71d
maxucheba@mail.ru <you@example.com>
2020-07-15 15:33:48 +0300

    str_func updated

--- src/ft_strcat.c
+++ src/ft_strcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vneelix <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: vneelix <vneelix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 21:36:41 by vneelix           #+#    #+#             */
-/*   Updated: 2019/09/12 15:27:43 by vneelix          ###   ########.fr       */
+/*   Updated: 2020/07/15 15:32:55 by vneelix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strcat(char *dest, const char *src)
 {
 	size_t	len[2];
 
+	if (!dest || !src)
+		return (NULL);
 	len[0] = ft_strlen(dest);
 	len[1] = ft_strlen(src);
 	ft_memcpy(dest + len[0], src, len[1]);
--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: vneelix <vneelix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 21:14:24 by vneelix           #+#    #+#             */
-/*   Updated: 2020/04/25 19:38:58 by vneelix          ###   ########.fr       */
+/*   Updated: 2020/07/15 15:32:26 by vneelix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*ret;
 	size_t	len[2];
 
+	if (!s1 || !s2)
+		return (NULL);
 	len[0] = ft_strlen(s1);
 	len[1] = ft_strlen(s2);
-	if ((ret = (char*)malloc(len[0] + len[1] + 1)) == NULL)
+	if (!(ret = ft_memalloc(len[0] + len[1] + 1)))
 		return (NULL);
-	ft_bzero(ret, len[0] + len[1] + 1);
 	ft_strcat(ret, s1);
 	ft_strcat(ret + len[0], s2);
 	return (ret);
