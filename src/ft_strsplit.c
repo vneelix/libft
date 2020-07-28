5bb044ad3902270005c420705615b9a0a02ce397
vneelix <maxucheba@mail.ru>
2020-07-28 19:30:24 +0300

    update split

--- src/ft_strsplit.c
+++ src/ft_strsplit.c
@@ -12,64 +12,41 @@
 
 #include "libft.h"
 
-static int	extract_words(char **ret, const char *s, char c)
+char	**memory_alloc(const char *string, const char separator)
 {
-	size_t	i;
-	char	*ptr;
+	size_t	size;
 
-	i = 0;
-	while (*s != '\0')
+	size = sizeof(char*);
+	while (*string)
 	{
-		while (*s == c)
-			s += 1;
-		ptr = (char*)(s);
-		while (*ptr != c && *ptr != '\0')
-			ptr += 1;
-		if (ptr != s && (ret[i] = ft_strndup(s, ptr - s)) == NULL)
-		{
-			while (i != 0)
-				free(ret[--i]);
-			return (-1);
-		}
-		s = ptr;
-		i += 1;
+		size += (*string && *string != separator) ? sizeof(char*) : 0;
+		while (*string && *string != separator)
+			string += 1;
+		string += *string ? 1 : 0;
 	}
-	return (0);
+	return (ft_memalloc(size));
 }
 
-static char	**create_ret(const char *s, char c)
+char	**ft_strsplit(const char *string, const char separator)
 {
-	size_t	i;
-	char	*ptr;
-	char	**ret;
+	size_t		i;
+	char		**ret;
+	const char	*temp;
 
+	if (!(ret = memory_alloc(string, separator)))
+		return (NULL);
 	i = 0;
-	while (*s != '\0')
+	while (*string)
 	{
-		while (*s == c)
-			s += 1;
-		i += (*s != '\0' ? 1 : 0);
-		ptr = (char*)(s);
-		while (*ptr != c && *ptr != '\0')
-			ptr += 1;
-		s = ptr;
-	}
-	if ((ret = (char**)malloc(sizeof(char*) * (i + 1))) == NULL)
-		return (NULL);
-	ft_bzero(ret, sizeof(char*) * (i + 1));
-	return (ret);
-}
-
-char		**ft_strsplit(const char *s, char c)
-{
-	char	**ret;
-
-	if ((ret = create_ret(s, c)) == NULL)
-		return (NULL);
-	if (extract_words(ret, s, c))
-	{
-		free(ret);
-		return (NULL);
+		while (*string && *string == separator)
+			string += 1;
+		temp = string;
+		while (*string && *string != separator)
+			string += 1;
+		if (temp != string)
+			if (!(ret[i++] = ft_strndup(temp, string - temp)))
+				return ft_nptr_del((void**)ret);
+		string += *string ? 1 : 0;
 	}
 	return (ret);
 }
